a8efd821585e5d908f0d28d36563cfbacc8b6425
sandhyarao24 <50480003+sandhyarao24@users.noreply.github.com>
2020-09-11 13:41:20 +0530

    Delete mmymodule.py

--- mmymodule.py
+++
@@ -1,8 +0,0 @@
-"""import"""
-def dummy():
-    """ def """  
-    print("hello")
-FRUITS = ["apple", "orange"]
-FRU = ("banana", "cherry")
-FRUIT = {"name":"vvv", "sname":"pydsdd"}
-FRUI = {"app", "jack"}
